40c6789856c42318480a4faea6bd797cb417ccdb
Fernando Dalai Aguilar Sanchez <ferdalaixd@gmail.com>
2023-02-20 20:56:17 -0600

    .

--- Basics/1.cpp
+++ Basics/1.cpp
@@ -1,1 +1,11 @@
-//Hi
+#include <iostream>  //Header file library that lets us work with input and output objects
+using namespace std;  //Means that we can use names for objects and variables from the standard library.
+
+int main(){
+
+    cout<<"Hello World";
+
+    return 0;
+}
+
+
