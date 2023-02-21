f014924344acc43d4233837aaf41378b632068bb
Fernando Dalai Aguilar Sanchez <ferdalaixd@gmail.com>
2023-02-21 14:10:12 -0600

    ..

--- Basics/1.cpp
+++ Basics/1.cpp
@@ -3,7 +3,31 @@ using namespace std;  //Means that we can use names for objects and variables fr
 
 int main(){
 
-    cout<<"Hello World";
+    // For comments
+
+    /*
+    For extense comments 
+    with more that 1 line
+    */
+
+
+
+    //Types of variables
+
+    int integer = 7;
+    double decimal = 6.99;
+    float large_decimal = 6.9999999999999999;
+    char character = "D";
+
+    //Console Output
+
+    cout << "Hello World" << endl;
+    cout << "I am learning C++ \n";
+    std::cout<<"Hello World";           //Without the "using namespace std"
+
+    cout << integer;
+
+    
 
     return 0;
 }
