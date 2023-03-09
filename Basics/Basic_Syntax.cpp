627cbb54eb91a5092cc7e3b14dd3878c99e231ad
Fernando Dalai Aguilar Sanchez <ferdalaixd@gmail.com>
2023-03-08 21:16:28 -0600

    .

--- Basics/Basic_Syntax.cpp
+++ Basics/Basic_Syntax.cpp
@@ -71,6 +71,17 @@ int main(){
 
     //Pointers
 
+    string food = "Pizza";  // A food variable of type string
+    string* ptr = &food;    // A pointer variable, with the name ptr, that stores the address of food
+
+    cout << food << "\n";   // Output the value of food (Pizza)
+    cout << &food << "\n";  // Output the memory address of food (0x6dfed4)
+    cout << ptr << "\n";    // Output the memory address of food with the pointer (0x6dfed4)
+        
+    *ptr = "Hamburger"; // Output the memory address of food with the pointer (0x6dfed4)
+    cout << *ptr << "\n";   // Output the new value of the pointer (Hamburger)
+
+
     //Console Output
 
     cout << "Hello World" << endl;
