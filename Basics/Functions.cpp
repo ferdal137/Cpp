3d94738ef14fdc10161af29b783e1ef1ca8c742f
Fernando Dalai Aguilar Sanchez <ferdalaixd@gmail.com>
2023-03-08 21:19:30 -0600

    .

---
+++ Basics/Functions.cpp
@@ -0,0 +1,11 @@
+// Create a function
+void myFunction() {
+  cout << "I just got executed!";
+}
+
+int main() {
+  myFunction(); // call the function
+  return 0;
+}
+
+// Outputs "I just got executed!"
