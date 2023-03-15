a63a644759f35b1bd4a65ddcac974ce7ae1ef452
Fernando Dalai Aguilar Sanchez <ferdalaixd@gmail.com>
2023-03-14 19:38:52 -0600

    .

--- Basics/Functions.cpp
+++ Basics/Functions.cpp
@@ -17,3 +17,20 @@ void myFunction() {
 }
 
 // Outputs "I just got executed!"
+
+
+// RECURSION
+
+int sum(int k) {
+  if (k > 0) {
+    return k + sum(k - 1);
+  } else {
+    return 0;
+  }
+}
+
+int main() {
+  int result = sum(10);
+  cout << result;
+  return 0;
+}
