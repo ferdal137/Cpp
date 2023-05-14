a51a80bad52b0a9731b45cecd93b766c43cce7cd
Fernando Dalai Aguilar Sanchez <ferdalaixd@gmail.com>
2023-05-14 10:49:50 -0600

    Constructors

---
+++ OPP/Constructors.cpp
@@ -0,0 +1,62 @@
+//A constructor in C++ is a special method that is automatically called when an object of a class is created.
+
+//To create a constructor, use the same name as the class, followed by parentheses ():
+
+#include <iostream>
+using namespace std;
+
+class MyClass {     // The class
+  public:           // Access specifier
+    MyClass() {     // Constructor
+      cout << "Hello World!";
+    }
+};
+
+int main() {
+  MyClass myObj;    // Create an object of MyClass (this will call the constructor)
+  return 0;
+}
+
+//Constructors can also take parameters (just like regular functions), which can be useful for setting initial values for attributes.
+
+class Car {        // The class
+  public:          // Access specifier
+    string brand;  // Attribute
+    string model;  // Attribute
+    int year;      // Attribute
+    Car(string x, string y, int z) { // Constructor with parameters
+      brand = x;
+      model = y;
+      year = z;
+    }
+};
+
+int main() {
+  // Create Car objects and call the constructor with different values
+  Car carObj1("BMW", "X5", 1999);
+  Car carObj2("Ford", "Mustang", 1969);
+
+  // Print values
+  cout << carObj1.brand << " " << carObj1.model << " " << carObj1.year << "\n";
+  cout << carObj2.brand << " " << carObj2.model << " " << carObj2.year << "\n";
+  return 0;
+}
+
+//Just like functions, constructors can also be defined outside the class.
+
+/*
+class Car {        // The class
+  public:          // Access specifier
+    string brand;  // Attribute
+    string model;  // Attribute
+    int year;      // Attribute
+    Car(string x, string y, int z); // Constructor declaration
+};
+
+// Constructor definition outside the class
+Car::Car(string x, string y, int z) {
+  brand = x;
+  model = y;
+  year = z;
+}
+*/
