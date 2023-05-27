030833856febf0aae805257b48af4b2e4c4b8b25
Fernando Dalai Aguilar Sanchez <ferdalaixd@gmail.com>
2023-05-26 22:46:30 -0600

    Arrays

--- Basics/Arrays.cpp
+++ Basics/Arrays.cpp
@@ -7,13 +7,15 @@ int main(){
     int myNum[3] = {10, 20, 30};
 
     cout << cars[1];             //Get an element
+    cout << "\n";
     cout << sizeof(myNum);
+    cout << "\n";
 
     string letters[2][4] = {
     { "A", "B", "C", "D" },      //Multi-Dimensional Arrays      
     { "E", "F", "G", "H" }
     };
-    
+
     cout << letters[0][2]; 
 
 
