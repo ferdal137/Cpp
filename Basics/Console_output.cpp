ba5804a14c1da1eb408e68cb82203c4adb6c71c4
Fernando Dalai Aguilar Sanchez <ferdalaixd@gmail.com>
2023-05-26 22:51:14 -0600

    Output

---
+++ Basics/Console_output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    int integer = 5;
+
+    cout << "Hello World" << endl;
+    cout << "\t I am learning C++ \n";
+    std::cout<<"Hello World \n";           //Without the "using namespace std"
+
+    cout << integer;
+    cout << "\n";
+    cout << "My favorite number is " << integer << " because...";
+
+}
