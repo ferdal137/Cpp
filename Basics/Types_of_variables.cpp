49196ed5688c36c25eabc8a160da9e7ba43b9f49
Fernando Dalai Aguilar Sanchez <ferdalaixd@gmail.com>
2023-05-26 22:42:31 -0600

    Variables

---
+++ Basics/Types_of_variables.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int integer = 7;
+    double decimal = 6.99;
+    float large_decimal = 6.9999999999999999;
+    char character = "D";
+    string text = "Hello World";
+    bool my_bool = true; 
+
+    int x = 10, y = 5, z = 2;
+
+    const float pi = 3.1416;  //A constant is a variable that can not chance its value 
+
+    float f1 = 35e3;      //Power of 10
+    double d1 = 12E4;
+
+
+}
