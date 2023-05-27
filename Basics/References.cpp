5ed3d1df9b9db91369e33c240bdabe30d16467d6
Fernando Dalai Aguilar Sanchez <ferdalaixd@gmail.com>
2023-05-26 22:47:50 -0600

    References

---
+++ Basics/References.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    string food = "Pizza";
+    string &meal = food;
+
+    cout << food << "\n";  // Outputs Pizza
+    cout << meal << "\n";  // Outputs Pizza
+    cout << &food; // Outputs 0x6dfed4
+
+
+}
